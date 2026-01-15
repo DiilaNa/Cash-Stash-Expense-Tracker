d07c53dbe6466cafc7b8fef5d715e87fb75802d2
DiilaNa <liyanaarachchidilan@gmail.com>
2026-01-15 17:40:35 +0530

    Connected FireBase

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,12 @@
-import "./global.css"
- 
-import React from 'react'
+import React from "react";
+import "../global.css";
+import { View, Text } from "react-native"; // Added Text here
 
 export default function index() {
   return (
-    <><h1>HI</h1></>
-  )
+    <View className="flex-1 justify-center items-center bg-white">
+      <Text className="text-xl font-bold text-black">Hi</Text>
+      <Text>fr</Text>
+    </View>
+  );
 }
